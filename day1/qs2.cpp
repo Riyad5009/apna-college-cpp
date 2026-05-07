a9b5bb3561c723bd86cf4fc3a1106c71011086fb
Abrar Riyad <rmd394195@gmail.com>
2026-05-07 21:45:35 +0600

    secondcommit

---
+++ day1/qs2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float pencil,pen,eraser;
+     cin>>pencil;
+     cin>>pen;
+     cin>>eraser;
+     float sum=pencil+pen+eraser;
+     cout<<"With vat:"<<sum;
+    return 0;
+}
